945ae2d46a4812a84afd6f9870b272e6cf4c3c1b
陈合龙 <chenhelong@mia.com>
2019-04-26 18:35:38 +0800

    add dist

---
+++ dist/precache-manifest.2d12675cd99d06f4182ae6a3d49ce160.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "95085a370ded877b3c3e",
+    "url": "/js/about.58eb088f.js"
+  },
+  {
+    "revision": "c4ce56788cfa236e326f",
+    "url": "/css/app.8942e36b.css"
+  },
+  {
+    "revision": "c4ce56788cfa236e326f",
+    "url": "/js/app.0cb9d2c6.js"
+  },
+  {
+    "revision": "df42b226518363954b4e",
+    "url": "/js/chunk-vendors.24f85c4f.js"
+  },
+  {
+    "revision": "20a3dbef1352b6bf5f0dca51ef5565e4",
+    "url": "/fonts/element-icons.20a3dbef.woff"
+  },
+  {
+    "revision": "597296c5a91daefe650a4fa20e2f65da",
+    "url": "/fonts/element-icons.597296c5.ttf"
+  },
+  {
+    "revision": "d7cacc0799d08d48284c89c712fdfa0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
